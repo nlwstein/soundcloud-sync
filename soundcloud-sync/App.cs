1a38c83a7d42dadf3611c23380dee81ecd5d285c
Nick Stein <nick@nickstein.net>
2014-08-13 01:09:53 -0400

    Bugfix: Commandline arguments

--- soundcloud-sync/App.cs
+++ soundcloud-sync/App.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.Concurrent;
+using System.Reflection;
 using System.Linq;
 using System.Text;
 using System.IO;
@@ -24,6 +25,7 @@ namespace soundcloud_sync
             Collection = GetUserContent(GetUserID(username), type);
             
             /* Let's concatenate our path. */
+            if (path == null) { path = Assembly.GetAssembly(typeof(App)).Location; }
 			path = Path.Combine(path,username,type);
             
             /* Download the collection! */
